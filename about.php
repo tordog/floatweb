97d4ee55ba5ac66508982ebbb97ef7bea4c7ba23
tordog <torie.nielsen@gmail.com>
2016-01-07 00:48:43 -0800

    Bios partially updated Spring 2016

--- about.php
+++ about.php
@@ -8,85 +8,120 @@
 	<div id="middlecontent">
 		<div id="aboutcontent">
 			<h1>ABOUT</h1>
-			<p>Float is an undergraduate organization that aims to bring more girls interested in technology and entrepreneurship into the Yale community. 
-				However, anyone is welcome to participate in its programs. It provides student-run workshops in web development and design to anyone interested, 
-				as well as a mentoring program to those considering the Computer Science major. All workshops and events are presented free-of-charge.
-			<br><br>To learn more, please <a href="./mailchimp_signup.php">sign up for the Float mailing list</a>, or email team@floatyale.com.</p>
+			<p>Float is an undergraduate organization that aims to bring more women interested in technology and entrepreneurship into the Yale community. 
+				However, anyone is welcome to participate in its programs. It provides various student-run workshops to anyone interested, 
+				as well as a mentoring program to those considering the Computer Science major. All workshops and events are presented free-of-charge. <br><br> As 
+				Float expands, we are eager to create and host more workshops, and send our members to conferences and hackathons. If you are interested in 
+				contributing or want to learn more, please <a href="./mailchimp_signup.php">sign up for the Float mailing list</a>, or email team@floatyale.com.</p>
 			<br><h1>MEET THE TEAM</h1>
 		</div>
 
 		<div id="meettheteam">
 
 
-			<p>Torie Nielsen BR'16, President <br><br><img src="./images/torie.jpg" alt="Torie" style="float:left; margin: 0 20px 0 0;
-				border:solid; border-color:white; border-width: 10px">
-			Torie is a Computer Science major from Palo Alto, CA. Apart from a lifetime of free Chinese food, 
-			her dream gift would be a collection of nice-writing pens, placed in the center of a basket filled 
-			with sea salt & caramel chocolates. Some other things she enjoys are running, baking, and art.
-			<br><br>To contact Torie please email her at torie@floatyale.com.</p><br><br>
-
-			<p>Brianna Loo MC'16, Vice President <br><br><img src="./images/brianna.jpg" alt="Brianna" style="float:left; margin: 0 20px 0 0;
-				border:solid; border-color:white; border-width: 10px">
-			Brianna is a senior electrical engineering and computer science major from South Pasadena, CA. 
-			When she's not running around taking photos for the YDN or staying up into the early hours of the
-			morning putting together tomorrow's paper, she enjoys baking chocolate desserts, traveling to warm places,
-			and dreaming of ways to keep a pet bunny in her room.
-			<br><br>To contact Brianna please email her at brianna@floatyale.com.</p> <br>
-
-			<p>Jessica Pancer SM'17, Mentorship Chair<br><br><img src="./images/jessica.jpg" alt="Jessica" style="float:left; margin: 0 20px 0 0;
-			border:solid; border-color:white; border-width: 10px">
-			Jessica is a junior in Silliman from Boca Raton, FL and a Computer Science major. When she isn't
-			dancing or watching Gilmore Girls reruns, she's either talking about computers or coding on one. 
-			She's really excited to be teaching the iOS course for Float!
-			</p>
-			<br>
-			<br>
-			<br>
-			<br>
-			<p>Payal Modi TD'17, Mentorship Chair<br><br><img src="./images/payal.jpg" alt="Payal" style="float:left; margin: 0 20px 0 0;
+			<p>Payal Modi TD '17, President <br><br><img src="./images/payal.jpg" alt="Payal" style="float:left; margin: 0 20px 0 0;
 			border:solid; border-color:white; border-width: 10px;width: 150px; height=150px; overflow: hidden">
 			Payal is a junior from Fremont, CA majoring in Computer Science and Economics. When she isn't coding, Payal is probably 
 			either singing or dancing. In her free time, Payal loves to watch Friends and Game of Thrones, look at pictures of baby hedgehogs, and eat pizza!
-			</p>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<p>Alicia Vargas-Morawetz JE'17, Events Chair<br><br><img src="./images/alicia.jpg" alt="Alicia" style="float:left; margin: 0 20px 0 0;
-			border:solid; border-color:white; border-width: 10px">	
+			<br><br>To contact Payal, please email her at payal.modi@yale.edu.</p><br><br><br><br>
+
+			<p>Alicia Vargas-Morawetz JE '17, Vice President <br><br><img src="./images/alicia.jpg" alt="Alicia" style="float:left; margin: 0 20px 0 0;
+			border:solid; border-color:white; border-width: 10px;width: 150px; height=150px; overflow: hidden">
 			Alicia is a computer science major from New York City and a junior in JE. Her favorite activities include 
 			eating pretzels and nutella while watching The West Wing, playing soccer, and rereading books she loves. 
-			On a free afternoon you can probably find her hanging out in a coffee shop or spending time outdoors with friends. 
-			</p>
-			<br> <br><br><br>
-			<p>Erika Hairston TD '18, Finance Chair <br><br><img src="./images/erika.jpg" alt="Erika" style="float:left; margin: 0 20px 0 0;
-			border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
-			Erika is a sophomore computer science major from Summit, NJ. She is on the Black Student Alliance at Yale board and 
-			tutors math for Urban Improvement Corps. In addition to being obsessed with Scandal, Erika loves to bake, play 
-			basketball, and scrapbook. She is extremely excited to be a part of Float Yale and to support more girls in technology. 
-			</p><br> <br><br><br><br>
-			<p>Alison Visher TC '18, Finance Chair <br><br><img src="./images/ali.jpg" alt="Alison" style="float:left; margin: 0 20px 0 0;
+			On a free afternoon you can probably find her hanging out in a coffee shop or spending time outdoors with friends.
+			<br><br>To contact Alicia, please email her at alicia.vargas-morawetz@yale.edu.</p><br><br>
+			
+			<p>Alison Visher TC '18, Mentorship Chair <br><br><img src="./images/ali.jpg" alt="Alison" style="float:left; margin: 0 20px 0 0;
 				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
 				Ali is a sophomore in Trumbull College. She is a computer science major. While she loves going to the beach and exploring 
 				the outdoors, most of the time she can be found on the couch eating some chocolate malted crunch ice cream and watching 
 				The Black List.
 			</p><br> <br><br><br><br>
+			<p>Annie Chen, Mentorship Chair <br><br><img src="./images/annie.jpg" alt="Alison" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Annie is a freshman in Stiles from San Ramon, California.  Although her mother still thinks she is an English Major, 
+				Annie is tentatively majoring in Computer Science, hopefully to work in educational technology. She feels really strongly 
+				about whipped cream, dinner, and her beloved Lineolated Parakeet, Reeny. She might be proudest of her Chrome Extension 
+				Catcaller (<a href="tiny.cc/catcaller">tiny.cc/catcaller</a>) [can this link be embedded]​, which shows you random cat GIFs. 
+				Download, rate and review to instantly become her friend.
+			</p><br> <br>
+			<p>Alex Saiontz, Mentorship Chair <br><br><img src="./images/alex.jpg" alt="Alison" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Alex is a CS major from Baltimore, MD and a sophomore in Branford. Alex is also a tour guide at the YUAG and has a radio
+				show called "Estrogin and Tonic". Alex has very strong opinions about food and New Haven restaurants and loves to talk about them. 
+			</p><br> <br><br><br><br>
+			<p>Anushree Agrawal, Events Chair <br><br><img src="./images/anushree.jpg" alt="Anushree" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Anushree is a freshman from Houston, TX, majoring in Computer Science. She loves watching lots of Netflix, especially Scandal 
+				and Grey's Anatomy. She also likes traveling to new places and eating lots of new foods, especially ice cream.
+			</p><br> <br><br><br><br>
+			<p>Saran Morgan, Events Chair <br><br><img src="./images/saran.jpg" alt="Saran" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Saran is a sophomore in Trumbull College; she's a Computer Science major from Miami, FL. She is interested in Cybersecurity 
+				and protection of sensitive information. In her free time, she does over 20 different styles of ballroom dance and take photos 
+				at Yale events. She is enthusiastic to be a part of Float and to encourage girls and women to enter and remain in STEM fields 
+				by creating a space where they can see themselves and their peers in successful technological positions and paths.
+			</p><br> <br>
+			<p>Aileen Huang, Finance Chair <br><br><img src="./images/aileen.jpg" alt="Aileen" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Aileen hails from the sleepy suburb of Plano, Texas and is currently pursuing the joint Computer Science and Mathematics major. 
+				While she's not pressing buttons on her computer to turn on tiny, tiny lights, she's probably singing in the shower, drawing people 
+				falling asleep in math class, or doing squats in the gym to get huge gainz. She's previously been a mentor for Float and a board member 
+				of WACSY, or Women Active in Computer Science at Yale, and is absolutely ecstatic to make a positive difference in Float in the 
+				upcoming months.
+			</p><br>
+			<p>Katie Watson, Publicity Chair <br><br><img src="./images/katie_watson.jpg" alt="KatieW" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; height=150px; width: 150px; overflow: hidden">
+				Katie is a sophomore from Los Angeles, CA majoring in Computer Science. Outside class, she's often found in the corner of a coffee 
+				shop with her sketchbook. She loves winter, and frequently binge-watches documentaries on mountaineering and Antarctica. She always 
+				orders her food extra-spicy, and is still on the lookout for the spiciest food in New Haven. </p><br><br><br><br>
+			<p>Sarah Sukin, Publicity Chair <br><br><img src="./images/sarah_sukin.jpg" alt="SarahS" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; height=150px; width: 150px; overflow: hidden">
+				Sarah is a sophomore from Atlanta, GA majoring in Computing and the Arts. If she's not in the graphic design studio or the zoo, 
+				you can often find her dreaming about warmer weather while building blanket forts with her suitemates. Sarah also enjoys raspberries, 
+				working to integrate the arts and sciences with STEAM, and the color maroon. </p><br><br><br><br><br>
 			<p>Lining Wang BK '17, Publicity Chair <br><br><img src="./images/lining.jpg" alt="Lining" style="float:left; margin: 0 20px 0 0;
 				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
-			Lining is a junior in Berkeley studying Computer Science. She has worked on the Dissent project at Yale, which aims
-			to preserve anonymity in an increasingly surveilled world using cryptography. She is also interested in applying 
-			technology to activism, especially to feminist, racial, and mental health issues.  In her free time, she likes to 
-			do graphic design, make comics and read books. </p><br><br><br><br><br>
+				Lining is a junior studying Computer Science. They like cryptography, distributed systems, and security/privacy. Last summer they
+				interned at Twitch, a video streaming company, and this summer they'll be at Slack as a security intern. They are particularly
+				interested in creating safe genderqueer spaces within tech. </p><br><br><br><br>
+			<p>Kristina Shia, Workshop Chair <br><br><img src="./images/kristina.jpg" alt="Kristina" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Kristina is a freshman in JE and a computer science major from Irvine, California. She's interested in Android development 
+				and game development. Kristina is a part of the YHack organizing team and Yale Women's Club Soccer. In her free time, she 
+				enjoys driving, eating, and driving to new places to eat.
+			</p><br> <br><br><br>
+			<p>Sarah Wagner, Workshop Chair <br><br><img src="./images/sarah_wagner.png" alt="SarahW" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Sarah Wagner is a freshman in Pierson College from the Washington D.C. Metropolitan area, with major currently undeclared. She 
+				loves chocolate of any kind, and in her free time she can be found in the pottery studio or making origami. She is thrilled to 
+				be a part of this organization and to promote females in computer science.
+			</p><br> <br><br><br>
+			<p>Ngan Vu, Website Chair <br><br><img src="./images/ngan.jpg" alt="Ngan" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Ngan is a freshman in Ezra Stiles from Hanoi, Vietnam. She hopes to major in either in Computer Science or Molecular, Cellular 
+				and Developmental Biology. Beside dabbling with codes, she also enjoys playing the piano, playing squash, taking pictures, light 
+				designing, and eating free food. </p><br><br><br><br><br><br>
 			<p style="font-size: 18px; color: rgba(62,80,163,1)">Alumni:</p>
-			<p>Christine Hong TC'15, Founder. <a href="www.linkedin.com/in/christinehong802">Contact Christine at LinkedIn</a>
+			<p>Christine Hong TC'15, Founder. <a href="www.linkedin.com/in/christinehong802">Contact Christine on LinkedIn</a>
  			<br><br><img src = "./images/christine-1.jpg" alt="Christine" style="float:left; margin: 0 20px 0 0;
 				border:solid; border-color:white; border-width: 10px; width: 150px; height: 150px; overflow:hidden"></p>
 
+			</br></br></br></br></br></br></br></br></br>
 			</br>
-			</br>
-			</br>
+		
+			<p>Torie Nielsen BR'16, Co-Founder. <a href="https://www.linkedin.com/in/victoria-nielsen-5bb22b55">Contact Torie on LinkedIn</a>
+ 			<br><br><img src = "./images/torie.jpg" alt="Torie" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height: 150px; overflow:hidden"></p>
+			<!--<p>Jessica Ambrosio, Workshop Chair <br><br><img src="./images/jessica_ambrosio.jpg" alt="JessicaA" style="float:left; margin: 0 20px 0 0;
+				border:solid; border-color:white; border-width: 10px; width: 150px; height=150px; overflow: hidden">
+				Aileen hails from the sleepy suburb of Plano, Texas and is currently pursuing the joint Computer Science and Mathematics major. 
+				While she's not pressing buttons on her computer to turn on tiny, tiny lights, she's probably singing in the shower, drawing people 
+				falling asleep in math class, or doing squats in the gym to get huge gainz. She's previously been a mentor for Float and a board member 
+				of WACSY, or Women Active in Computer Science at Yale, and is absolutely ecstatic to make a positive difference in Float in the 
+				upcoming months.
+			</p><br> <br><br><br><br>-->
 		</div>
 
 	</div>
